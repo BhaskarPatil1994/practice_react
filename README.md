706431c86804e059b435e51fbce4e69833eeb6bd
Bhaskar_Patil <bhaskarpatil166199@gmail.com>
2026-04-01 17:50:20 +0530

    raw code

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 <!-- #react practice code file -->
+
+# parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File watching algorithm - written in C++
+- Caching - faster builds
+- Image Optimization
+- Bundling your file 
+- Compress your file 
+- consistent Hashing 
+- Code Splitting for you
+- Differntial Bundling - support older browsers
+- Diagnostic 
+- Error handling 
+- to host app on HTTPS 
+- Tree shaking algorithm - will remove unused code for you
--- app.js
+++ app.js
@@ -7,10 +7,13 @@
 </div> */
 }
 
+import React from "react";
+import ReactDOM from "react-dom/client";        
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+    React.createElement("h1", {}, "this is pune"),
+    React.createElement("h2", {}, "page "),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am h1 tag"),
